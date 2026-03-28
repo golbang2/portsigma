5dd528835d650c1d91fce826d8eec36d0533df2f
taeyoon kim <32033086+golbang2@users.noreply.github.com>
2026-03-28 17:53:13 +0900

    fix cors for vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "https://doublerock.io",
         "https://portsigma-hm3qus1jr-golbang2s-projects.vercel.app/",  # 실제 vercel 주소로 바꿔라
+        "https://portsigma.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
