1a45c0b68c85d12a3fc7fcd26e91d17bbca071c1
taeyoon kim <32033086+golbang2@users.noreply.github.com>
2026-03-28 17:10:08 +0900

    Fix CORS for production domains

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,9 +14,10 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://doublerock.io",
+        "https://portsigma-hm3qus1jr-golbang2s-projects.vercel.app/",  # 실제 vercel 주소로 바꿔라
     ],
-    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1)(:\d+)?$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
